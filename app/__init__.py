f8b4962066b81c84f25ac46bb51634192caff081
Gabriel Wachira <gabbyk.com@gmail.com>
2023-11-23 07:44:21 +0300

    update type hinting, docstrings, error handling

--- app/__init__.py
+++ app/__init__.py
@@ -3,30 +3,50 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from storage.db_storage import DbStorage
 from storage.config import Config
+from typing import Optional
 
 db = SQLAlchemy()
 
-def create_app(config_name="development"):
-    """Create an app instance."""
+def create_app(config_name: Optional[str] = "development"):
+    """Create an app instance.
+    
+    :param config_name: The name of the configuration to use.
+    :type config_name: Optional[str]
+    :return: The created Flask app instance.
+    :rtype: Flask
+    """
+
     app = Flask(__name__)
     #app.config.from_object(configurations[config_name])
     app.config.from_object(Config)
 
-    db.init_app(app)
+    try:
+        db.init_app(app)
+    except Exception as e:
+        print(f"Failed to initialize database: {e}")
+        return None
 
     with app.app_context():
-        # Register database
-        # db = SQLAlchemy(app)
-        # storage = DbStorage(db)
+        try:
+            from models.patient import Patient
+            from models.provider import Provider
+            db.create_all()
+        except Exception as e:
+            print(f"Failed to create database tables: {e}")
+            return None
 
-        from models.patient import Patient
-        db.create_all()
-
-        storage = DbStorage(db)
-        app.config['storage'] = storage
+        try:
+            storage = DbStorage(db)
+            app.config['storage'] = storage
+        except Exception as e:
+            print(f"Failed to initialize storage engine: {e}")
+            return None
 
         # Register routes and views blueprint
-        from api.v1.views import api_bp
-        app.register_blueprint(api_bp)
-
+        try:
+            from api.v1.views import api_bp
+            app.register_blueprint(api_bp)
+        except Exception as e:
+            print(f"Failed to register blueprint: {e}")
+            return None
     return app
--- storage/db_storage.py
+++ storage/db_storage.py
@@ -1,47 +1,99 @@
+
+from flask_sqlalchemy import SQLAlchemy
 """Database storage engine module."""
 
+
 class DbStorage:
-    """Database storage engine class."""
+    """Database storage engine class.
+
+    Attributes:
+        db (SQLAlchemy): The SQLAlchemy instance.
+    """
     db = None
 
-    def __init__(self, db):
-        """Initialize the database storage engine."""
-        self.db = db
+    def __init__(self, db: SQLAlchemy):
+        """Initialize the database storage engine.
+
+        Args:
+            db (SQLAlchemy): A SQLAlchemy instance.
+        """
+        if db is None:
+            self.db = None
+        else:
+            self.db = db
 
     def all(self, cls=None) -> list:
-        """Return all objects of a given class."""
-        return self.db.session.query(cls).all()
+        """Return all objects of a given class.
+
+        Args:
+            cls (type, optional): The class to query. Defaults to None.
+
+        Returns:
+            list: A list of objects.
+        """
+        if self.db is None:
+            return []
+        else:
+            return self.db.session.query(cls).all()
 
     def new(self, obj):
-        """Add a new object to the database."""
-        self.db.session.add(obj)
+        """Add a new object to the database.
+
+        Args:
+            obj: The object to add.
+        """
+        if self.db is not None:
+            self.db.session.add(obj)
 
     def save(self):
         """Save changes to the database."""
-        self.db.session.commit()
+        if self.db is not None:
+            self.db.session.commit()
 
     def delete(self, obj=None):
-        """Delete an object from the database."""
-        if obj is not None:
+        """Delete an object from the database.
+
+        Args:
+            obj: The object to delete.
+        """
+        if self.db is not None and obj is not None:
             self.db.session.delete(obj)
 
     def reload(self):
         """Create all tables in the database."""
-        self.db.create_all()
+        if self.db is not None:
+            self.db.create_all()
 
     def close(self):
         """Close the database session."""
-        self.db.session.close()
+        if self.db is not None:
+            self.db.session.close()
 
-    def get(self, cls, id):
-        """Get an object from the database."""
-        return self.db.session.query(cls).get(id)
+    def get(self, cls, id) -> object:
+        """Get an object from the database.
 
-    def count(self, cls=None):
-        """Count the number of objects in storage."""
-        session = Session(self.db)
-        if cls is None:
-            return session.query(cls).count()
+        Args:
+            cls (type): The class of the object.
+            id: The ID of the object.
+
+        Returns:
+            object: The object with the specified ID.
+        """
+        if self.db is None:
+            return None
         else:
-            return session.query(cls).count()
+            return self.db.session.query(cls).get(id)
+
+    def count(self, cls=None) -> int:
+        """Count the number of objects in storage.
+
+        Args:
+            cls (type, optional): The class to count. Defaults to None.
+
+        Returns:
+            int: The number of objects.
+        """
+        if self.db is None:
+            return 0
+        else:
             return self.db.session.query(cls).count()
