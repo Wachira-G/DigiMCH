475fa4f1c2641f250ab2a250c67d6013b0f8ce0e
Gabriel Wachira <gabbyk.com@gmail.com>
2023-11-23 17:35:44 +0300

    fix typo

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,5 +4,5 @@ api_bp = Blueprint("api_bp", __name__, url_prefix="/api/v1")
 
 from api.v1.views.index import *
 from api.v1.views.patient import *
-from api.v
+from api.v1.views.provider import *
 from api.v1.views.error_handlers import *
