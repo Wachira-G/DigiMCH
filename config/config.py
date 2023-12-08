f395e2e699843e06fc96118e9f0b9ad0a04f7b65
Gabriel Wachira <gabbyk.com@gmail.com>
2023-12-08 15:28:49 +0300

    Update Profile - heroku gunicorn, config

--- config/config.py
+++ config/config.py
@@ -44,7 +44,7 @@ class TestConfig(Config):
 
 class ProductionConfig(Config):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = postgresdb_config
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
 class DevelopmentConfig(Config):
